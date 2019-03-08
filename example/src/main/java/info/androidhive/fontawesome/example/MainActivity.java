f991d01671bd101839cb2c02490c61e71f88af48
Ravi Tamada <ravi8x@gmail.com>
2019-03-08 12:56:26 +0530

    Added fa icons to navigation drawer menu items

--- example/src/main/java/info/androidhive/fontawesome/example/MainActivity.java
+++ example/src/main/java/info/androidhive/fontawesome/example/MainActivity.java
@@ -2,26 +2,41 @@ package info.androidhive.fontawesome.example;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
-import info.androidhive.fontawesome.FontCache;
 import info.androidhive.fontawesome.FontDrawable;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        initFab();
+        intDrawerLayout();
+    }
+
+    /***
+     * Changing FAB icon. FontDrawable is used to change the fab icon
+     */
+    private void initFab() {
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,15 +45,57 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
         FontDrawable drawable = new FontDrawable(this, R.string.fa_paper_plane_solid, true, false);
         drawable.setTextColor(ContextCompat.getColor(this, android.R.color.white));
         fab.setImageDrawable(drawable);
+    }
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        FontDrawable drawableUp = new FontDrawable(this, R.string.fa_home_solid, true, false);
-        drawableUp.setTextColor(ContextCompat.getColor(this, android.R.color.white));
-        drawableUp.setTextSize(25);
-        toolbar.setNavigationIcon(drawableUp);
+    /**
+     * Changing navigation drawer icons
+     * This involves looping through menu items and applying icons
+     */
+    private void intDrawerLayout() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ImageView iconHeader = navigationView.getHeaderView(0).findViewById(R.id.nav_header_icon);
+        FontDrawable drawable = new FontDrawable(this, R.string.fa_font_awesome, false, true);
+        drawable.setTextColor(ContextCompat.getColor(this, android.R.color.white));
+        drawable.setTextSize(50);
+        iconHeader.setImageDrawable(drawable);
+
+        int[] icons = {
+                R.string.fa_home_solid, R.string.fa_calendar_alt_solid, R.string.fa_user_solid,
+                R.string.fa_heart_solid, R.string.fa_comment_solid, R.string.fa_dollar_sign_solid, R.string.fa_gift_solid
+        };
+        renderMenuIcons(navigationView.getMenu(), icons, true, false);
+
+        int[] iconsSubmenu = {R.string.fa_cog_solid, R.string.fa_sign_out_alt_solid};
+
+        renderMenuIcons(navigationView.getMenu().getItem(7).getSubMenu(), iconsSubmenu, true, false);
+    }
+
+    /**
+     * Looping through menu icons are applying font drawable
+     */
+    private void renderMenuIcons(Menu menu, int[] icons, boolean isSolid, boolean isBrand) {
+        for (int i = 0; i < menu.size(); i++) {
+            MenuItem menuItem = menu.getItem(i);
+            if (!menuItem.hasSubMenu()) {
+                FontDrawable drawable = new FontDrawable(this, icons[i], isSolid, isBrand);
+                drawable.setTextColor(ContextCompat.getColor(this, R.color.icon_nav_drawer));
+                drawable.setTextSize(22);
+                menu.getItem(i).setIcon(drawable);
+            }
+        }
     }
 
     @Override
@@ -61,5 +118,30 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
